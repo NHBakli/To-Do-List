a4f2778b337a3f18ff33e163889cd813c931e536
NH Bakli <bakli25@protonmail.com>
2023-12-08 18:27:30 +0100

    Réalisation de la création des table en PDO

--- config/create_table.php
+++ config/create_table.php
@@ -12,16 +12,21 @@ class CreateTable {
         
         $connexion = $this->db->getConnection();
 
-        $sql = "CREATE TABLE IF NOT EXISTS users (
-            id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-            username VARCHAR(128) NOT NULL,
-            email VARCHAR(256) NOT NULL,
-            password VARCHAR(256) NOT NULL)";
+        try{
+            $sql = "CREATE TABLE IF NOT EXISTS users (
+                id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                username VARCHAR(128) NOT NULL,
+                email VARCHAR(256) NOT NULL,
+                password VARCHAR(256) NOT NULL)";
 
-        if (mysqli_query($connexion, $sql)) {
-            echo '<br> La table users a été créée avec succès ! </br>';
-        } else {
-            mysqli_error($connexion, $sql);
+            $stmt = $connexion->prepare($sql);
+
+            if ($stmt->execute()){
+                echo '<br> La table users a été créée avec succès ! </br>';
+            }
+        }
+        catch(PDOException $e) {
+            echo "Erreur PDO : " . $e->getMessage();
         }
 
     }
@@ -30,33 +35,42 @@ class CreateTable {
         
         $connexion = $this->db->getConnection();
 
-        $sql = "CREATE TABLE IF NOT EXISTS list (
-            id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-            id_user INT(10) NOT NULL,
-            title VARCHAR(255) NOT NULL)";
+        try{
+            $sql = "CREATE TABLE IF NOT EXISTS list (
+                id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                id_user INT(10) NOT NULL,
+                title VARCHAR(255) NOT NULL)";
 
-        if (mysqli_query($connexion, $sql)) {
-            echo '<br> La table list a été créée avec succès ! </br>';
-        } else {
-            mysqli_error($connexion, $sql);
+            $stmt = $connexion->prepare($sql);
+
+            if ($stmt->execute()){
+                echo '<br> La table users a été créée avec succès ! </br>';
+            }
         }
-
-    }
+        catch(PDOException $e) {
+            echo "Erreur PDO : " . $e->getMessage();
+        }
+}
 
     public function createtasksTable() {
         
         $connexion = $this->db->getConnection();
 
-        $sql = "CREATE TABLE IF NOT EXISTS task (
-            id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
-            id_list INT(10) NOT NULL,
-            description text NOT NULL,
-            completed BOOLEAN NOT NULL DEFAULT 0)";
+        try{
+            $sql = "CREATE TABLE IF NOT EXISTS task (
+                id INT(10) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                id_list INT(10) NOT NULL,
+                description text NOT NULL,
+                completed BOOLEAN NOT NULL DEFAULT 0)";
 
-        if (mysqli_query($connexion, $sql)) {
-            echo '<br> La table list a été créée avec succès ! </br>';
-        } else {
-            mysqli_error($connexion, $sql);
+            $stmt = $connexion->prepare($sql);
+
+            if ($stmt->execute()){
+                echo '<br> La table users a été créée avec succès ! </br>';
+            }
+        } 
+        catch(PDOException $e) {
+            echo "Erreur PDO : " . $e->getMessage();
         }
 
     }
